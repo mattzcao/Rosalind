9b44125ad3cf1191d044d36d7065c3a9a78c1913
Matthew Cao <34463179+Noobly387@users.noreply.github.com>
2020-07-29 14:43:32 -0700

    Corrected Line Iteration
    Originally i % 2 ==0, this returns the odd lines since zero-based index.
    i % 2 == 1 will return the even lines. 0 = 1st line, 1 = 2nd line (even)

--- Python Village/Problem 5/ini5.py
+++ Python Village/Problem 5/ini5.py
@@ -3,12 +3,9 @@ arr = [];
 with open ("rosalind_ini5.txt", "r") as f:
     for line in f.readlines():
         arr.append(line)
-#    while str != None:
-#        arr.append(str);
-#        str = line.readline()
 
-for i in range(len(arr)):
-    if i % 2 == 0:
+for i in len(arr):
+    if i % 2 == 1:
         print arr[i]
 
 
